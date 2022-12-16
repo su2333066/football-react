5b0f12c0feb7ba067cf976c8adfc34c6d0691703
박기수 <gisu130@gmail.com>
2022-12-16 15:23:05 +0900

    navbar 디자인

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -4,7 +4,39 @@ import { StoreContext } from "../App";
 function Main() {
   const { loginUser } = React.useContext(StoreContext);
 
-  return <div>안녕하세요 {loginUser.nickname}님~</div>;
+  return (
+    <div className="container">
+      <div className="navbar">
+        <div className="navContainer">
+          <div className="navLogo">
+            <a href="/">
+              <img
+                src="https://plab-football.s3.amazonaws.com/static/img/logo.svg"
+                alt="플랩풋볼"
+              ></img>
+            </a>
+          </div>
+          <div className="navRight">
+            <div className="navSearch">
+              <button>
+                <img src="https://plab-football.s3.amazonaws.com/static/img/ic_search.svg"></img>
+              </button>
+              <input
+                type="search"
+                maxlength="100"
+                autocomplete="off"
+                placeholder="지역, 구장 이름으로 찾기"
+              ></input>
+            </div>
+            <button>
+              <img src="https://plab-football.s3.amazonaws.com/static/img/ic_my.svg"></img>
+            </button>
+            <button>•••</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Main;
